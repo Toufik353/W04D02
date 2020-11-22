3c8b2e7f5337138f0d11b5c4ee15568169994e67
Toufik Desai <60398527+Toufik353@users.noreply.github.com>
2020-11-22 21:03:07 +0530

    Add files via upload

---
+++ problem4.js
@@ -0,0 +1,21 @@
+//Print the total population of countries using reduce function
+
+var request=new XMLHttpRequest;
+
+request.open("GET","https://restcountries.eu/rest/v2/all",true);
+
+request.send();
+
+request.onload=function(){
+    try{
+
+    var data=JSON.parse(this.response);
+    var totalPopulation=data.reduce((acc,item)=>{
+        return acc+item.population
+    },0)
+    console.log(totalPopulation)
+    }
+    catch(err){
+        console.log("err");
+    }
+}
